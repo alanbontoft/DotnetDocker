08876f32c9307a47f63410c8e617646a87806b9c
alanbontoft <alan.bontoft@gmail.com>
2024-09-02 21:37:14 +0100

    Initial source commit

---
+++ src/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace DotnetDocker;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Hello Docker");
+
+        await Task.Run(async () =>
+        {
+            int i=0;
+            while (true)
+            {
+                Console.WriteLine($"Hello from docker {i++}");
+                await Task.Delay(1000);
+            }
+        });
+    }
+}
